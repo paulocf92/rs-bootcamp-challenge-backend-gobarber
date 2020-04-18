c72364688d7c64722a2cd5f7fc3f3b01b37f4df3
Paulo Filho <pcf.feat@gmail.com>
2020-04-18 11:53:16 -0400

    chore: draft transactions import service

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,58 @@
+import fs from 'fs';
+import parse from 'csv-parse';
+import path from 'path';
+import uploadConfig from '../config/upload';
+
+import CreateTransactionService from './CreateTransactionService';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(csvFilename: string): Promise<void> {
+    const createTransaction = new CreateTransactionService();
+
+    const csvFilePath = path.join(uploadConfig.directory, csvFilename);
+
+    const transactions = [] as Transaction[];
+    const fileData = await fs.promises.readFile(csvFilePath);
+
+    await new Promise(resolve =>
+      parse(fileData, {}, async (err, rows) => {
+        if (err) {
+          throw new Error(err.message);
+        }
+
+        const lines = rows.slice(1);
+
+        await Promise.all(
+          lines.map(async (row: string[]) => {
+            const [title, type, value, category] = row;
+            const transactionData = {
+              title,
+              type: type.trim(),
+              value: parseFloat(value),
+              category: category.trim(),
+            } as TransactionDTO;
+
+            const transaction = await createTransaction.execute(
+              transactionData,
+            );
+
+            transactions.push(transaction);
+          }),
+        );
+
+        resolve(transactions);
+      }),
+    ).then(results => {
+      return results;
+    });
   }
 }
 
